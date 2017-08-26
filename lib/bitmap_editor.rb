d74de3d94d061cdf520890c2fd212cb6d1f5a549
Kanmani <kanmaniselvan@gmail.com>
2017-08-26 14:40:54 +0530

    Add bitmap reader writer methods module
    * BitmapReadWriterMethods module will provide the methods for the six commands and included that in the BitmapEditor class.
    * Remove double quotes in the puts statements of run method of BitmapEditor class.

--- lib/bitmap_editor.rb
+++ lib/bitmap_editor.rb
@@ -1,14 +1,15 @@
 class BitmapEditor
+  include BitmapReaderWriterMethods
 
   def run(file)
-    return puts "please provide correct file" if file.nil? || !File.exists?(file)
+    return puts 'please provide correct file' if file.nil? || !File.exists?(file)
 
-    File.open(file).each do |line|
-      line = line.chomp
+    File.open(file).each do |_line|
+      line = _line.chomp
       case line
-      when 'S'
-          puts "There is no image"
-      else
+        when 'S'
+          puts 'There is no image'
+        else
           puts 'unrecognised command :('
       end
     end
---
+++ lib/bitmap_reader_writer_methods.rb
@@ -0,0 +1,19 @@
+module BitmapReaderWriterMethods
+  def create_m_x_n_image_file(m, n)
+  end
+
+  def color_x_y_pixels(x, y, color)
+  end
+
+  def color_vertical_segments(x, y1, y2, color)
+  end
+
+  def color_horizontal_segments(x1, x2, y, color)
+  end
+
+  def show_the_file_contents
+  end
+
+  def clear_the_file_contents
+  end
+end
