f334b710fe5095f79fbf97118ac7e593bdbeca3e
Kanmani <kanmaniselvan@gmail.com>
2017-08-27 09:32:22 +0530

    Split large spec into separate specs
    * Split the larger spec file into commands_spec and reader_writer_spec under bitmap_editor folder. Since the file seems to be growing big.

--- spec/models/bitmap_editor/commands_spec.rb
+++ spec/models/bitmap_editor/commands_spec.rb
@@ -1,40 +1,9 @@
-require 'pry'
 require './errors/feedback_error'
 require './lib/bitmap_command_validator'
 require './lib/bitmap_reader_writer_methods'
 require './lib/bitmap_editor'
 
 RSpec.describe BitmapEditor do
-  context 'Responds to it\'s instance methods' do
-    before(:all) do
-      @bitmap_editor = BitmapEditor.new
-    end
-
-    it 'responds to create_m_x_n_bitmap_image_text_file method' do
-      expect(@bitmap_editor).to respond_to(:create_m_x_n_bitmap_image_text_file).with(2).arguments
-    end
-
-    it 'responds to color_x_y_pixels method' do
-      expect(@bitmap_editor).to respond_to(:color_x_y_pixels).with(4).arguments
-    end
-
-    it 'responds to color_vertical_segments method' do
-      expect(@bitmap_editor).to respond_to(:color_vertical_segments).with(4).arguments
-    end
-
-    it 'responds to color_horizontal_segments method' do
-      expect(@bitmap_editor).to respond_to(:color_horizontal_segments).with(4).arguments
-    end
-
-    it 'responds to show_the_file_contents method' do
-      expect(@bitmap_editor).to respond_to(:show_the_file_contents)
-    end
-
-    it 'responds to clear_the_file_contents method' do
-      expect(@bitmap_editor).to respond_to(:clear_the_file_contents)
-    end
-  end
-
   context 'It performs all inputs commands' do
     before(:each) do
       @bitmap_editor = BitmapEditor.new
